362072ee1ad5cccabeeb1b8193cda6205ca94e95
kite1412 <frozradit@gmail.com>
2026-03-18 03:09:48 +0800

    chore: bump Kotlin, Gradle, AGP, and dependency versions

--- app/src/main/java/nrr/konnekt/ui/KonnektApp.kt
+++ app/src/main/java/nrr/konnekt/ui/KonnektApp.kt
@@ -21,7 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.compose.rememberNavController
